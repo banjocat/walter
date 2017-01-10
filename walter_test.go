ed9a8d191599ba7f176dc10abe80d8263ac1f2d4
banjocat <jackmuratore@gmail.com>
2017-01-10 00:01:42 -0500

    Add tests for multiple ips

--- walter_test.go
+++ walter_test.go
@@ -14,9 +14,27 @@ func TestSSH(t *testing.T) {
 		Port: 2222,
 	}
 	responses := SSH(config, "whoami")
-	assert.Equal(t, responses[0].ip, "jackmuratore.com", "Should be jackmuratore.com")
-	assert.Equal(t, responses[0].stdout, "root", "Should output root")
-	assert.Equal(t, responses[0].stderr, "", "There should be no error")
-	assert.Equal(t, responses[0].errorCode, 0, "Should exit with 0")
+	assert.Equal(t, responses[0].ip, "jackmuratore.com", "IP should be returned")
+	assert.Equal(t, responses[0].stdout, "root", "stdout should be result of command")
+	assert.Equal(t, responses[0].stderr, "", "Make sure there are no errors")
+	assert.Equal(t, responses[0].errorCode, 0, "Returns 0 if no error")
+}
 
+func TestMultipleSSH(t *testing.T) {
+	ips := []string{"jackmuratore.com", "jackmuratore.com", "jackmuratore.com"}
+	config := &Config{
+		Pem:  "~/.ssh/id_rsa",
+		Ips:  ips,
+		User: "root",
+		Port: 2222,
+	}
+	responses := SSH(config, "whoami")
+	assert.Equal(t, 3, len(responses), "Check if returns the correct number of responses")
+	for i := 0; i < len(responses); i++ {
+
+		assert.Equal(t, responses[i].ip, "jackmuratore.com", "IP should be returned")
+		assert.Equal(t, responses[i].stdout, "root", "stdout should be result of command")
+		assert.Equal(t, responses[i].stderr, "", "Make sure there are no errors")
+		assert.Equal(t, responses[i].errorCode, 0, "Returns 0 if no error")
+	}
 }
