acaea2376a1e57211369dc7b0b4ece0da4820680
banjocat <jackmuratore@gmail.com>
2017-01-14 13:46:59 -0500

    Add test for DNS and IP, also add output

--- walter.go
+++ walter.go
@@ -4,104 +4,106 @@ Package walter implements a simple way to ssh multiple hosts at once
 package walter
 
 import (
-    "golang.org/x/crypto/ssh"
-    "io/ioutil"
-    "bytes"
-    "sync"
-    "log"
-    "strings"
-    "fmt"
+	"bytes"
+	"fmt"
+	"golang.org/x/crypto/ssh"
+	"io/ioutil"
+	"log"
+	"strings"
+	"sync"
 )
-
 
 /*
 Config is the struct used to setup an SSH request
 */
 type Config struct {
-    Pem  string
-    Ips  []string
-    Port int
-    User string
+	Pem         string
+	Ips         []string
+	Port        int
+	User        string
+	PrintOutput bool // Outputs results to stdout
 }
 
 /*
 Response is the slice returned from SSH
 */
 type Response struct {
-    ip        string
-    stderr    string
-    stdout    string
+	ip     string
+	stderr string
+	stdout string
 }
 
 /*
 SSH is the main endpoint to send ssh commands
 */
-func SSH(config *Config, command string) ([]*Response) {
-    clientConfig, err := walterConfigToCyrptoConfig(config)
-    if err != nil {
-	return nil
-    }
-    var wg sync.WaitGroup
-    responses := make(chan *Response, len(config.Ips))
-    for _, ip := range config.Ips {
-	wg.Add(1)
-	go runOneSSH(clientConfig, ip, config.Port, command, responses, &wg)
-    }
-    wg.Wait()
-    close(responses)
-    var response_slice []*Response
-    for elem := range responses {
-	response_slice = append(response_slice, elem)
-    }
-    return response_slice
+func SSH(config *Config, command string) []*Response {
+	clientConfig, err := walterConfigToCyrptoConfig(config)
+	if err != nil {
+		return nil
+	}
+	var wg sync.WaitGroup
+	responses := make(chan *Response, len(config.Ips))
+	for _, ip := range config.Ips {
+		wg.Add(1)
+		go runOneSSH(clientConfig, ip, config.Port, command, responses, &wg)
+	}
+	wg.Wait()
+	close(responses)
+	var response_slice []*Response
+	for elem := range responses {
+	    if config.PrintOutput {
+		fmt.Printf("[%s] (%s) %s\n", elem.ip, command, elem.stdout)
+	    }
+		response_slice = append(response_slice, elem)
+	}
+	return response_slice
 }
 
-func runOneSSH(clientConfig *ssh.ClientConfig, host string, port int, command string, responses chan <- *Response, wg *sync.WaitGroup) {
-    defer wg.Done()
-    hostname := fmt.Sprintf("%s:%d", host, port)
-    client, err := ssh.Dial("tcp", hostname, clientConfig)
-    if err != nil {
-	log.Fatalf("Fatal client: %s", err)
-	return
-    }
-    session, err := client.NewSession()
-    if err != nil {
-	log.Fatalf("Fatal session: %s", err)
-	return
-    }
-    defer session.Close()
-    var stdout bytes.Buffer
-    var stderr bytes.Buffer
-    session.Stdout = &stdout
-    session.Stderr = &stderr
-    if err := session.Run(command); err != nil {
-	log.Fatalf("Fatal run: %s", err)
-	return
-    }
-    response := &Response {
-	ip: host,
-	stderr: strings.TrimSpace(stderr.String()),
-	stdout: strings.TrimSpace(stdout.String()),
-    }
-    responses <- response
+func runOneSSH(clientConfig *ssh.ClientConfig, host string, port int, command string, responses chan<- *Response, wg *sync.WaitGroup) {
+	defer wg.Done()
+	hostname := fmt.Sprintf("%s:%d", host, port)
+	client, err := ssh.Dial("tcp", hostname, clientConfig)
+	if err != nil {
+		log.Fatalf("Fatal client: %s", err)
+		return
+	}
+	session, err := client.NewSession()
+	if err != nil {
+		log.Fatalf("Fatal session: %s", err)
+		return
+	}
+	defer session.Close()
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	session.Stdout = &stdout
+	session.Stderr = &stderr
+	if err := session.Run(command); err != nil {
+		log.Fatalf("Fatal run: %s", err)
+		return
+	}
+	response := &Response{
+		ip:     host,
+		stderr: strings.TrimSpace(stderr.String()),
+		stdout: strings.TrimSpace(stdout.String()),
+	}
+	responses <- response
 }
 
 func walterConfigToCyrptoConfig(config *Config) (*ssh.ClientConfig, error) {
-    key, err := ioutil.ReadFile(config.Pem)
-    if err != nil {
-	log.Fatal("Fatal: %s", err)
-	return nil, err
-    }
-    signer, err := ssh.ParsePrivateKey(key)
-    if err != nil {
-	return nil, err
-    }
-    clientConfig := &ssh.ClientConfig{
-	User: config.User,
-	Auth: []ssh.AuthMethod{
-	    ssh.PublicKeys(signer),
-	},
-    }
-    return clientConfig, nil
+	key, err := ioutil.ReadFile(config.Pem)
+	if err != nil {
+		log.Fatal("Fatal: %s", err)
+		return nil, err
+	}
+	signer, err := ssh.ParsePrivateKey(key)
+	if err != nil {
+		return nil, err
+	}
+	clientConfig := &ssh.ClientConfig{
+		User: config.User,
+		Auth: []ssh.AuthMethod{
+			ssh.PublicKeys(signer),
+		},
+	}
+	return clientConfig, nil
 }
-
--- walter_test.go
+++ walter_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestSSH(t *testing.T) {
+func TestSSHwithIP(t *testing.T) {
 	ips := []string{"107.170.57.50"}
 	config := &Config{
 		Pem:  "/home/banjocat/.ssh/id_rsa",
@@ -16,15 +16,51 @@ func TestSSH(t *testing.T) {
 	responses := SSH(config, "whoami")
 	assert.Equal(t, len(responses), 1, "Should find just 1 response")
 	if len(responses) != 1 {
-	    return
+		return
+	}
+	assert.Equal(t, responses[0].ip, "107.170.57.50", "IP should be returned")
+	assert.Equal(t, responses[0].stdout, "root", "stdout should be result of command")
+	assert.Equal(t, responses[0].stderr, "", "Make sure there are no errors")
+}
+
+func TestMultipleSSHwithIP(t *testing.T) {
+	ips := []string{"107.170.57.50", "107.170.57.50", "107.170.57.50"}
+	config := &Config{
+		Pem:  "/home/banjocat/.ssh/id_rsa",
+		Ips:  ips,
+		User: "root",
+		Port: 2222,
+	}
+	responses := SSH(config, "whoami")
+	assert.Equal(t, 3, len(responses), "Check if returns the correct number of responses")
+	for i := 0; i < len(responses); i++ {
+
+		assert.Equal(t, responses[i].ip, "107.170.57.50", "IP should be returned")
+		assert.Equal(t, responses[i].stdout, "root", "stdout should be result of command")
+		assert.Equal(t, responses[i].stderr, "", "Make sure there are no errors")
+	}
+}
+
+func TestSSHwithDNS(t *testing.T) {
+	ips := []string{"jackmuratore.com"}
+	config := &Config{
+		Pem:  "/home/banjocat/.ssh/id_rsa",
+		Ips:  ips,
+		User: "root",
+		Port: 2222,
+	}
+	responses := SSH(config, "whoami")
+	assert.Equal(t, len(responses), 1, "Should find just 1 response")
+	if len(responses) != 1 {
+		return
 	}
 	assert.Equal(t, responses[0].ip, "jackmuratore.com", "IP should be returned")
 	assert.Equal(t, responses[0].stdout, "root", "stdout should be result of command")
 	assert.Equal(t, responses[0].stderr, "", "Make sure there are no errors")
 }
 
-func TestMultipleSSH(t *testing.T) {
-	ips := []string{"107.170.57.50", "107.170.57.50", "107.170.57.50"}
+func TestMultipleSSHwithDNS(t *testing.T) {
+	ips := []string{"jackmuratore.com", "jackmuratore.com", "jackmuratore.com"}
 	config := &Config{
 		Pem:  "/home/banjocat/.ssh/id_rsa",
 		Ips:  ips,
